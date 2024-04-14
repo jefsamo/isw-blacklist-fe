b781434b427e614e3b259454e6c6f3b2ccd7176e
Olawale Shopeyin <shopeyinwale@gmail.com>
2024-04-14 21:24:44 +0100

    image upload refactor

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -6,6 +6,9 @@ import {
   Space,
   TextInput,
   rem,
+  Badge,
+  FileButton,
+  Text,
 } from "@mantine/core";
 import { IconAt } from "@tabler/icons-react";
 
@@ -13,20 +16,22 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useUser } from "../../hooks/useUser";
 import { useState } from "react";
 import { useUpdateUser } from "../../hooks/useUpdateUser";
+import axios from "axios";
+import { BASE_URL } from "../../constants";
 
 const User = () => {
   const { userId } = useParams();
   const navigate = useNavigate();
   const icon = <IconAt style={{ width: rem(16), height: rem(16) }} />;
 
-  const { isLoading, user } = useUser();
-  const [userEmail, setUserEmail] = useState(user?.data.email ?? "");
-  const [firstName, setFirstName] = useState(user?.data.firstName ?? "");
-  const [lastName, setLastName] = useState(user?.data.lastName ?? "");
-  const [imageUrl, setImageUrl] = useState(user?.data.imageUrl ?? ""); // Assuming you want this to be controlled
-  const [role, setRole] = useState(user?.data.role ?? "");
-
   const { updateUser, isPending } = useUpdateUser();
+  const { isLoading, user } = useUser();
+
+  const [userEmail, setUserEmail] = useState(user?.data.email);
+  const [firstName, setFirstName] = useState(user?.data.firstName);
+  const [lastName, setLastName] = useState(user?.data.lastName);
+  const [imageUrl, setImageUrl] = useState(user?.data.imageUrl);
+  const [role, setRole] = useState(user?.data.role ?? "");
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setUserEmail(e.target.value);
@@ -36,13 +41,41 @@ const User = () => {
     setLastName(e.target.value);
   const handleImageUrlChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setImageUrl(e.target.value);
-  // const handleRoleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-  //   setRole(e.target.value);
+  const [isUploading, setIsUploading] = useState(false);
+
+  const [file, setFile] = useState<File | null>(null);
+  const currentUser = JSON.parse(localStorage.getItem("user")!);
 
   const isEdit =
     firstName === user?.data.firstName &&
     lastName === user?.data.lastName &&
     imageUrl === user?.data.imageUrl;
+
+  const handleUpload = async () => {
+    if (file) {
+      const formData = new FormData();
+      formData.append("photo", file);
+
+      try {
+        setIsUploading(true);
+        const response = await axios.post<{ url: string }>(
+          `${BASE_URL}/items/photo`,
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              Authorization: `Bearer ${currentUser?.jwToken}`,
+            },
+          }
+        );
+        setIsUploading(false);
+        setImageUrl(response.data.url);
+      } catch (error) {
+        setIsUploading(false);
+        console.error("Error uploading image:", error);
+      }
+    }
+  };
 
   const handleUpdateUser = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -101,13 +134,44 @@ const User = () => {
         onChange={handleLastNameChange}
       />
       <Space h="sm" />
-      <TextInput
-        label="Image Url"
-        placeholder="Image"
-        value={imageUrl}
-        w={300}
-        onChange={handleImageUrlChange}
-      />
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <TextInput
+          label="Image Url"
+          placeholder="Image"
+          value={imageUrl}
+          w={300}
+          onChange={handleImageUrlChange}
+        />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <FileButton
+            onChange={setFile}
+            accept="image/png,image/jpeg,image/jpg"
+          >
+            {(props) => (
+              <Badge
+                {...props}
+                color="blue"
+                style={{ marginTop: "20px", cursor: "pointer" }}
+              >
+                choose photo
+              </Badge>
+            )}
+          </FileButton>
+          {file && (
+            <Text size="sm" ta="center" mt="sm">
+              {file.name}
+            </Text>
+          )}
+          <Button
+            style={{ marginTop: "20px" }}
+            onClick={handleUpload}
+            loading={isUploading}
+          >
+            Upload
+          </Button>
+        </div>
+      </div>
+
       <Space h="sm" />
       <Select
         label="Role"
